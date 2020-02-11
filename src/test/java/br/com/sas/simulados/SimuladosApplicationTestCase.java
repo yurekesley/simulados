7ce2a7e7e8e165badd5c34e0cffe014dd8bd3e29
Yure Kesley Moreira Plácido <yure.placido@softplan.com.br>
2020-02-11 16:14:28 -0300

    adicionando dbuinit manager

---
+++ src/test/java/br/com/sas/simulados/SimuladosApplicationTestCase.java
@@ -0,0 +1,8 @@
+package br.com.sas.simulados;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = { SimuladosApplication.class })
+public abstract class SimuladosApplicationTestCase {
+
+}
